e003d276428207d4e8545083bf7e5d90ddd05258
Abd Elrhman Mohamed Shata <abdelrhmanmohamedshata@gmail.com>
2024-07-18 21:22:22 +0300

    First Commit

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+     'author': 'AbdElrhman.M.Shata',
+    'summary': 'Hospital Management is currently unavailable and will be updated soon in the future',
+    'description': """ Hospital Management System """,
+    'depends': ['mail','product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/male_patient_view.xml',
+        'views/appoinment_view.xml',
+        ],
+    'demo':[],
+    'sequence':-100,
+    'application': True,
+    # 'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ custom_addons/om_hospital/models/appointment.py
@@ -0,0 +1,93 @@
+from odoo import models,fields,api
+ 
+class HospitalAppointment(models.Model):
+    _name="hospital.appointment"
+    _inherit=["mail.thread","mail.activity.mixin"]
+    _description="Hospital Appointment"
+    _rec_name="ref"
+    
+    patient_id = fields.Many2one(comodel_name='hospital.patient',string="Patient")
+    patient_gender=fields.Selection(related='patient_id.gender',readonly=True) 
+      
+    appointment_time=fields.Datetime(string='Appointment Time',default=lambda self: fields.Datetime.now())
+    booking_date = fields.Date(string="Booking Date",default=fields.Date.context_today)
+    ref = fields.Char(string="Reference",default="Odoo Mates",help="Reference to the patient record")
+    prescription=fields.Html(string="Prescription")
+    
+    docter_id = fields.Many2one('res.users',string="Docter")
+    
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Favorite'),
+    ], default='0', string="Priority")
+    
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled'),
+    ], default='draft',string="State",required=True)
+    
+    
+    pharmacy_line_ids = fields.One2many('appointment.pharmacy.lines','appointment_id',string="Pharmacy lines")
+    
+        
+    @api.onchange('patient_id')
+    def onChange_patient_id(self):
+        self.ref = self.patient_id.ref
+        
+    
+    def action_test(self):
+        return {
+                'effect': {
+                    'fadeout': 'slow',
+                    'message': "Click Successfully",
+                    # 'img_url': '/web/image/%s/%s/image_1024' % (self.team_id.user_id._name, self.team_id.user_id.id) if self.team_id.user_id.image_1024 else '/web/static/img/smile.svg',
+                    'type': 'rainbow_man',
+                }
+            }
+        
+        
+    
+    def action_draft(self):
+        for rec in self:
+            rec.state = 'draft'
+ 
+    def action_in_consultation(self):
+        for rec in self:
+            rec.state = 'in_consultation'
+    
+    def action_done(self):
+        for rec in self:
+            rec.state = 'done'
+            
+    def action_cancel(self):
+        for rec in self:
+            rec.state = 'cancel'
+    
+    
+    
+
+class AppointmentPharmacyLines(models.Model):
+    _name="appointment.pharmacy.lines"
+    _description="Appointment Pharmacy Lines"
+    
+    product_id = fields.Many2one("product.product",required=True)
+    price_unit = fields.Float(related='product_id.list_price', string="Price")
+    qty = fields.Integer(string="Quantity",default=1) 
+    appointment_id = fields.Many2one('hospital.appointment',string="Appointment")    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
